bd1092b286f8c2b38562f947f9d425b7fee51a1a
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-11-12 00:12:04 +0300

    Feeding animals created

---
+++ Feed the Animals/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour {
+    private float _topBound = 30f;
+    private float _lowerBound = -10f;
+    
+    void Update() {
+        if (transform.position.z > _topBound) {
+            Destroy(gameObject);
+        } else if (transform.position.z < _lowerBound) {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Feed the Animals/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public GameObject[] animalPrefabs;
+    private float _spawnRangeX = 20f;
+    private float _spawnPosZ = 20f;
+    private float _startDelay = 2;
+    private float _spawnInterval = 1.5f;
+    
+    void Start() {
+        InvokeRepeating("SpawnRandomAnimal", _startDelay, _spawnInterval);
+    }
+
+    void SpawnRandomAnimal() {
+        var animalIndex = Random.Range(0, animalPrefabs.Length);
+        var spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
