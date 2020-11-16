86a4064e68897709dadd8c0526cd7244dcefa8c4
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-11-17 00:57:58 +0300

    Run and death animations added

--- Jumping over obstacles/Assets/Scripts/PlayerController.cs
+++ Jumping over obstacles/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,21 @@ public class PlayerController : MonoBehaviour {
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator _animator;
     
     // Start is called before the first frame update
     void Start() {
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
             _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            _animator.SetTrigger("Jump_trig");
         }
     }
 
@@ -30,6 +33,8 @@ public class PlayerController : MonoBehaviour {
         } else if (other.gameObject.CompareTag("Obstacle")) {
             gameOver = true;
             Debug.Log("Game Over!");
+            _animator.SetBool("Death_b", true);
+            _animator.SetInteger("DeathType_int", 1);
         }
     }
 }
