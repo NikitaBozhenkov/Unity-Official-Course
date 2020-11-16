a1c3b67e0201355e24ed8eb5af9f4c96301d32bb
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-11-17 00:11:54 +0300

    Added simple game over mechanic

--- Jumping over obstacles/Assets/Scripts/PlayerController.cs
+++ Jumping over obstacles/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    private bool _gameOver = false;
     
     // Start is called before the first frame update
     void Start() {
@@ -24,6 +25,11 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void OnCollisionEnter(Collision other) {
-        isOnGround = true;
+        if (other.gameObject.CompareTag("Ground")) {
+            isOnGround = true;
+        } else if (other.gameObject.CompareTag("Obstacle")) {
+            _gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
