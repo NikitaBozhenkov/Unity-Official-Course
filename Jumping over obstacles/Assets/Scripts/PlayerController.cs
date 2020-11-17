d3cd3cb0ad4667d77ecc895bb608c6efd05524da
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-11-17 03:20:18 +0300

    Effects added

--- Jumping over obstacles/Assets/Scripts/PlayerController.cs
+++ Jumping over obstacles/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,18 @@ public class PlayerController : MonoBehaviour {
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private AudioSource _audioSource;
     private Animator _animator;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     
     // Start is called before the first frame update
     void Start() {
         _rb = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -24,17 +30,23 @@ public class PlayerController : MonoBehaviour {
             _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             _animator.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            _audioSource.PlayOneShot(jumpSound, 1f);
         }
     }
 
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.CompareTag("Ground")) {
             isOnGround = true;
-        } else if (other.gameObject.CompareTag("Obstacle")) {
+            dirtParticle.Play();
+        } else if (other.gameObject.CompareTag("Obstacle") && !gameOver) {
             gameOver = true;
             Debug.Log("Game Over!");
             _animator.SetBool("Death_b", true);
             _animator.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            _audioSource.PlayOneShot(crashSound, 1f);
         }
     }
 }
