4cfcc12d5070aa7e3cd7d1da977ae9af8ccddd0b
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-11-18 15:47:51 +0300

    Added camera rotation and player movement

---
+++ Sumo Battle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody _rb;
+    private GameObject _focalPoint;
+    public float speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start() {
+        _rb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var verticalInput = Input.GetAxis("Vertical");
+        
+        _rb.AddForce(_focalPoint.transform.forward * (speed * verticalInput));
+    }
+}
