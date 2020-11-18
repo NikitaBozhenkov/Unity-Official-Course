7d080533b663c603fb00d2ac81778d73382001bb
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-11-18 16:39:11 +0300

    Added enemy spawning

---
+++ Sumo Battle/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    private float _speed = 3f;
+    private Rigidbody _rb;
+    private GameObject _player;
+    
+    // Start is called before the first frame update
+    void Start() {
+        _rb = GetComponent<Rigidbody>();
+        _player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var lookDirection = (_player.transform.position - transform.position).normalized;
+        _rb.AddForce(lookDirection * _speed);
+    }
+}
---
+++ Sumo Battle/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public GameObject enemyPrefab;
+    private float _spawnRange;
+
+    // Start is called before the first frame update
+    void Start() {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition() {
+        float spawnPosX = Random.Range(-9, 9);
+        float spawnPosZ = Random.Range(-9, 9);
+        
+        var randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
