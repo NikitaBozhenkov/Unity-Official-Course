50967ee299b244c8c3fb34f2072c7124cb73a9a9
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2020-12-07 01:35:07 +0300

    Fixed all physics

--- CourseProject/Assets/Scripts/MoveBack.cs
+++ CourseProject/Assets/Scripts/MoveBack.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBack : MonoBehaviour {
-    [SerializeField] private Vector3 moveDirection;
     [SerializeField] private float speed;
-    [SerializeField] private float deleteZ = -8;
+    [SerializeField] private Vector3 moveDirection;
+    private Rigidbody _rb;
+    
+    // Start is called before the first frame update
+    void Start() {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
-    void Update() {
-        if (gameObject.CompareTag("Wall")) {
-            if (transform.position.z < -10) {
-                var pos = transform.position;
-                pos.z = 90;
-                transform.position = pos;
-            }
-        } else {
-            if (transform.position.z < deleteZ) {
-                Destroy(gameObject);
-            }
-        }
-        
-        transform.Translate(moveDirection * (speed * Time.deltaTime));
+    void FixedUpdate() {
+        var vel = _rb.velocity;
+        vel.z = moveDirection.z * speed;
+        _rb.velocity = vel;
+        //_rb.AddForce(moveDirection * speed, ForceMode.Force);
     }
 }
---
+++ CourseProject/Assets/Scripts/MoveWall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour {
+    [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private float speed;
+
+    void Update() {
+        if (transform.position.z < -10) {
+            var pos = transform.position;
+            pos.z = 90;
+            transform.position = pos;
+        }
+
+        transform.Translate(moveDirection * (speed * Time.deltaTime));
+    }
+}
